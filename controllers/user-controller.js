fab9a048b92a1b1570c81f2b2054f81f8ac87346
jlewisit <joelewis.mortgage@gmail.com>
2022-11-01 20:19:39 -0700

    user-controller near completion

---
+++ controllers/user-controller.js
@@ -0,0 +1,71 @@
+const { create } = require('domain');
+const { User, Comment } = require('../models');
+
+const userController = {
+    getUsers(req, res) {
+        User.find({})
+        .select('-__v')
+        .sort({ _id: -1 })
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => {
+            console.log(err);
+            res.sendStatus(400);
+        });
+    },
+
+    getUserByID({ params }, res) {
+        User.findOne({ _id: params.id })
+        .populate({
+            path: 'comments',
+            select: '-__v'
+        })
+        .populate({
+            path: 'friends',
+            select: '-__v'
+        })
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({ message: 'No user found with that id' });
+                return;                
+            }
+            res.json(dbUserData);
+        })
+    .catch(err => {
+        res.sendStatus(400);
+    });      
+}
+}
+
+createUser({ body }, res) {
+    User.create(body)
+    .then(dbUserData => res.json(dbUserData))
+    .catch(err => res.json(err));
+}
+
+updateUser({ params, body }, res) {
+    User.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
+.then(dbUserData => {
+    if (!dbUserData) {
+        res.status(404).json({ message: 'No userFound with that id' });
+        return;
+    }
+    res.json(dbUserData);
+})
+.catch(err => res.json(err));
+},
+
+removeUser({ params }, res) {
+    Comment.deleteMany({ userId: params.id})
+    .then(() => {
+        User.findOneAndRemove({ userId: params.id })
+        .then(dbUserData => {
+            res.status(404).json({ message: 'No user found with that id' });
+            return;
+        }
+        res.json(dbUserData);
+        });
+    })
+    .catch(err => res.json(err));
+},
+
+addFriend
